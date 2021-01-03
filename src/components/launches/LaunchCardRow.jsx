502517c74256f3485e0676184ea0a3c9e2ffeffd
casualorange <casualoranges@gmail.com>
2021-01-03 22:06:00 +0000

    Disable launches that do not have a description

--- src/components/launches/LaunchCardRow.jsx
+++ src/components/launches/LaunchCardRow.jsx
@@ -3,17 +3,21 @@ import { bestDateTime } from '../../util/date';
 import { LaunchIndicators } from './LaunchIndicators';
 import { useHistory } from 'react-router-dom';
 
-const handlePush = (e, push, id, launch) => {
-    if ((e.target.id !== 'icon-tooltip')) {
-        push(`/launch/${id}`, { launch: launch });
+const handlePush = (e, push, id, launch, shouldLock) => {
+    if (!shouldLock) {
+        if ((e.target.id !== 'icon-tooltip')) {
+            push(`/launch/${id}`, { launch: launch });
+        }
     }
 };
+
 export const LaunchCardRow = (props) => {
-    const { id, name, date_utc, date_precision, success, payloads } = props.launch;
+    const { id, name, date_utc, date_precision, success, payloads, details } = props.launch;
     const customers = payloads.map(p => p.customers.join(', ')).flat();
     const { push } = useHistory();
-
-    return <li className='list-group-item' onClick={(e) => handlePush(e, push, id, props.launch)} >
+    const shouldLock = (details === null || details === "");
+    const disabledClass = (shouldLock) ? `disable-row` : false;
+    return <li className={`list-group-item ${disabledClass}`} onClick={(e) => handlePush(e, push, id, props.launch, shouldLock)} >
         <div className='row'>
             <h6 className='col'>
                 {name}
